46664993b7da3a8ac2648455cd85abc875a194ea
Th0rvar <theokt62@gmail.com>
2022-04-04 21:57:57 +0200

    fix: qtarget

--- [dependency]/qtarget/html/js/script.js
+++ [dependency]/qtarget/html/js/script.js
@@ -1,66 +1,60 @@
-
-
-window.addEventListener('message', function(event) {
+window.addEventListener('message', function (event) {
 	let item = event.data;
 
 	if (item.response == 'openTarget') {
-		$(".target-label").html("");
-
+		$('.target-label').html('');
 		$('.target-wrapper').show();
-
-		//$(".target-eye").css("color", "black");
-		$(".target-eye").css("display", "block");
-		$("#dot").css("display", "none");
+		$('.target-eye').css('color', 'black');
 	} else if (item.response == 'closeTarget') {
-		$(".target-label").html("");
-
+		$('.target-label').html('');
 		$('.target-wrapper').hide();
 	} else if (item.response == 'validTarget') {
-		$(".target-label").html("");
+		$('.target-label').html('');
 
-		$.each(item.data, function (index, item) {
-			$(".target-label").append("<div class='target-item' id='target-"+index+"'<li><i class='"+item.icon+" fa-fw fa-pull-left target-icon'></i>"+item.label+"</li></div>");
-			$("#target-"+index).hover((e)=> {
-				$("#target-"+index).css("color",e.type === "mouseenter"?"rgb(13, 156, 25)":"white")//rgb(98, 135, 236)
-			})
-			
-			$("#target-"+index+"").css("padding-top", "7px");
-
-			$("#target-"+index).data('TargetData', item);
+		Object.values(item.data).forEach((item, index) => {
+			index++;
+			$('.target-label').append(
+				`<div class='target-item' id='${index}'><i class='${item.icon} fa-fw fa-pull-left target-icon'></i>${item.label}</div>`
+			);
+			$(`#target-${index}`).hover((e) => {
+				$(`#target-${index}`).css('color', e.type === 'mouseenter' ? 'rgb(98, 135, 236)' : 'white');
+			});
+			$('#' + index).css('padding-top', '0.75vh');
 		});
 
-		//$(".target-eye").css("color", "rgba(255, 255, 255, 0.8)");
-		$(".target-eye").css("display", "none");
-		$("#dot").css("display", "block");
+		$('.target-eye').css('color', 'rgba(255, 255, 255, 0.8)');
 	} else if (item.response == 'leftTarget') {
-		$(".target-label").html("");
-		$(".target-eye").css("display", "block");
-
-		$("#dot").css("display", "none");
+		$('.target-label').html('');
+		$('.target-eye').css('color', 'black');
 	}
 });
 
 $(document).on('mousedown', (event) => {
-	let element = event.target;
-	let split = element.id.split("-")
-	
-	if (split[0] === 'target') {
-		$(".target-label").html("");
-		$('.target-wrapper').hide();
-		switch (event.which) {
-			case 1:
-				$.post('http://qtarget/selectTarget', JSON.stringify(Number(split[1]) + 1));
-			case 3:
-				$.post('http://qtarget/closeTarget');
+	switch (event.which) {
+		case 1: {
+			const id = event.target.id
+			if (id) $.post(`https://${GetParentResourceName()}/selectTarget`, JSON.stringify(Number(id)));
+			$('.target-label').html('');
+			$('.target-wrapper').hide();
+			break;
+		}
+		case 3: {
+			$.post(`https://${GetParentResourceName()}/leftTarget`);
+			$('.target-label').html('');
+			$('.target-eye').css('color', 'black');
 			break;
 		}
 	}
 });
 
-$(document).on('keydown', function(e) {
-	if (e.key == 'Escape') {
-		$(".target-label").html("");
-		$('.target-wrapper').hide();
-		$.post('http://qtarget/closeTarget');
+$(document).on('keydown', (event) => {
+	switch (event.key) {
+		case 'Escape':
+		case 'Backspace': {
+			$.post(`https://${GetParentResourceName()}/closeTarget`);
+			$('.target-label').html('');
+			$('.target-wrapper').hide();
+			$('.target-eye').css('color', 'black');
+		}
 	}
 });
