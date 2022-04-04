2bc9b9b0f84dd07d76bd4b2c852ed4d8f4c12306
Yolac 57 <ivelol@yahoo.fr>
2022-04-04 10:19:20 +0200

    Update cl_clothes.lua

--- Ora/gameplay/shops/clothes/cl_clothes.lua
+++ Ora/gameplay/shops/clothes/cl_clothes.lua
@@ -1329,7 +1329,7 @@ Citizen.CreateThread(
                                                 Clothes.type = Clothes.category[i].type
                                                 Clothes.remM = Clothes.category[i].remM
                                                 Clothes.remF = Clothes.category[i].remF
-
+                                                i = 0
                                                 Clothes.listLabel = {}
                                                 if Clothes.type == 0 then
                                                     Clothes.amount =
@@ -1350,7 +1350,7 @@ Citizen.CreateThread(
                                                     Clothes.static = Clothes.category[i].staticF
                                                 end
                                                 Clothes.Indexes = {}
-                                                for i = 0, in ipairs(Clothes.static) do
+                                                for i, in pairs(Clothes.static) do
                                                     Clothes.Indexes[i] = 1
                                                 end
                                             end
