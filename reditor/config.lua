024063db72b035379fe4ffdc9b41940e2f2c4102
Stealing <tom.dupont6470@gmail.com>
2022-04-04 21:27:26 +0200

    delete resources editor

--- reditor/config.lua
+++
@@ -1,9 +0,0 @@
-config = {
-	binding = {
-		start_record_replay = 288, -- F1		OFF
-		start_record = 163, -- 9
-		stop_save_record = 163, -- 9
-		stop_discard_record = 344, -- F11
-		open_editor = 162 -- 8							OFF (Already on esc menu)
-	}
-}
--- reditor/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'adamant'
-
-game 'gta5'
-
-client_scripts {
-	"config.lua",
-	"client.lua"
-}
