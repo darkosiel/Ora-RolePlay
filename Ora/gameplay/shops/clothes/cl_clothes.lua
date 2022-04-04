2358cea6ccd5e415f5179114eb7012dbd6cf15dc
Stealing <tom.dupont6470@gmail.com>
2022-04-04 17:01:10 +0200

    eeee

--- Ora/gameplay/shops/clothes/cl_clothes.lua
+++ Ora/gameplay/shops/clothes/cl_clothes.lua
@@ -699,7 +699,7 @@ local Clothes = {
             staticM = json.decode(Config.Ears),
             staticF = json.decode(Config.EarsF)
         },
-        {Label = "Montres",component = 6,type = 1,staticM = {},staticF = {}},
+        {Label = "Montres",component = 6,type = 0,staticM = {},staticF = {}},
         {label = "Sac", component = 5, type = 0, staticM = {}, staticF = {}}
     }
 }
