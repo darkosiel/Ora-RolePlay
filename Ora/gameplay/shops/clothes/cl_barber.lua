95f1f545f256906ad3474ccd209eb86f2cef65ff
Yolac 57 <ivelol@yahoo.fr>
2022-04-04 22:40:26 +0200

    Update cl_barber.lua

--- Ora/gameplay/shops/clothes/cl_barber.lua
+++ Ora/gameplay/shops/clothes/cl_barber.lua
@@ -192,11 +192,10 @@ local locked_Maquillage = true
 local locked_Blush = true
 local locked_rouge = true
 local function Open()
-    hrtfgdhdfhgc = PlySkin
     skins.hair = PlySkin.hair
     skins.facialHair = PlySkin.facial.hair
     skins.makeup = PlySkin.makeup
-    skins.blush = Plyskin.blush
+    skins.blush = PlySkin.blush
     skins.lipstick = PlySkin.lipstick
     (PlySkin.hair.style)
     (skins.hair.style)
