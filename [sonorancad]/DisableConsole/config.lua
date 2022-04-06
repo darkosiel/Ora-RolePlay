7d7095f7749c458958e0aadcd0daf1cdcc88432d
Th0rvar <theokt62@gmail.com>
2022-04-06 14:40:26 +0200

    rm: useless files

--- [sonorancad]/DisableConsole/config.lua
+++
@@ -1,7 +0,0 @@
-Config = Config or {}
-
-Config.Whitelist = {
-  'license:a53b28a3941e94455c72e4bce74eb9ac0ee86af9', -- Thorvar
-  'license:jbhj5bl3jh5vbl3hjhvl34543623kgjkhgkfds', -- Yolac
-  'license:25acce66a6ce9adcad46997588614f4d999ceddf', -- Stealing
-}
