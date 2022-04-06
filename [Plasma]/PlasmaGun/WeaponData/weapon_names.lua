967cd57b2203b6ec2d775ab7d172bb78f96727f2
Th0rvar <theokt62@gmail.com>
2022-04-06 13:35:06 +0200

    test car names

--- [Plasma]/PlasmaGun/WeaponData/weapon_names.lua
+++ [Plasma]/PlasmaGun/WeaponData/weapon_names.lua
@@ -1,7 +1,4 @@
 Citizen.CreateThread(function()
-
     AddTextEntry("WT_PLASMAP", "Plasma Pistol")
     AddTextEntryByHash(GetHashKey("WT_PLASMAP"),"Plasma Pistol")
-
-    AddTextEntry('PSCOUT', 'Scout 2014 LSPD')
 end)
