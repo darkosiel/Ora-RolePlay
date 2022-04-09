73e1fabec7700dfeaff45688e327abae8fbc6a27
Yolac 57 <ivelol@yahoo.fr>
2022-04-09 12:24:13 +0200

    loading

--- loadscreen/__resource.lua
+++ loadscreen/__resource.lua
@@ -1,11 +1,21 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
 files {
-    'index.html',
-    'keks.css',
-    'bankgothic.ttf',
-    'loadscreen.jpg',
-    'music/Loading.ogg',
+    'loadscreen.html',
+
+    'css/loadscreen.css',
+
+    'img/cursor.png',
+    'img/logo.png',
+    'img/logonoel.png',
+    'img/colorLayer.png',
+    'img/unmute.png',
+    'img/mute.png',
+
+    'medias/music.mp3',
+    'medias/video.webm',
+
+    'js/loadscreen.js'
 }
 
-loadscreen 'index.html'
-
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+loadscreen 'loadscreen.html'
