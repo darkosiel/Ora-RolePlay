d5e8f487e21f5757c9ba976b054f2ce9d5b570b4
Stealing <tom.dupont6470@gmail.com>
2022-04-10 15:28:37 +0200

    Delete Editor in core

--- Ora/gameplay/civilian/editor/config.lua
+++
@@ -1,9 +0,0 @@
-config = {
-	binding = {
-		start_record_replay = 288, -- F1
-		start_record = 163, -- F9
-		stop_save_record = 163, -- F9
-		stop_discard_record = 344, -- F2
-		open_editor = 162 -- F11
-	}
-}
