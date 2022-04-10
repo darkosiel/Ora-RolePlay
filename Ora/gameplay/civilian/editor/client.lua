215d4a36e412510a66503b6a8b670d08e20d98b7
Stealing <tom.dupont6470@gmail.com>
2022-04-10 15:03:48 +0200

    Editor

--- Ora/gameplay/civilian/editor/client.lua
+++ Ora/gameplay/civilian/editor/client.lua
@@ -1,23 +1,23 @@
-Citizen.CreateThread(
-	function()
-		while true do
-			Wait(10)
-			if(IsRecording()) then
-				if(IsControlJustPressed(1, config.binding.stop_save_record)) then
-					ShowNotification('~g~Enregistrement sauvegardé')
-					StopRecordingAndSaveClip()
-				end
-
-				if(IsControlJustPressed(1, config.binding.stop_discard_record)) then
-					ShowNotification('~r~Enregistrement annulé')
-					StopRecordingAndDiscardClip()
-				end
-			else
-				if(IsControlJustPressed(1, config.binding.start_record)) then
-					ShowNotification('~b~Enregistrement en cours')
-					StartRecording(1)
-				end
+Citizen.CreateThread(function ()
+	while(true) do
+		Citizen.Wait(10)
+		if(IsRecording()) then
+			if(IsControlJustPressed(1,config.binding.stop_save_record)) then
+				StopRecordingAndSaveClip()
+			end
+			
+			if(IsControlJustPressed(1,config.binding.stop_discard_record)) then
+				StopRecordingAndDiscardClip()
+			end
+		else
+			--Doesn't seems to work
+			if(IsControlJustPressed(1,config.binding.start_record_replay)) then
+				StartRecording(0)
+			end
+		
+			if(IsControlJustPressed(1,config.binding.start_record)) then
+				StartRecording(1)
 			end
 		end
 	end
-)
+end)
