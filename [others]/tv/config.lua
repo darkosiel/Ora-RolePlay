1c02bc5730d075bedf4373346d8587621f93e9eb
Th0rvar <theokt62@gmail.com>
2022-04-14 14:25:59 +0200

    fix: tv scale

--- [others]/tv/config.lua
+++ [others]/tv/config.lua
@@ -24,7 +24,7 @@ Config['Objects'] = {
     {
         ['Object'] = 'ex_prop_ex_tv_flat_01',
         ['Scale'] = 0.053,
-        ['Offset'] = vec3(-0.918, -0.055, 1.0),
+        ['Offset'] = vec3(-0.918, -0.055, 1.1),
         ['Distance'] = 25.5,
     },
     {
