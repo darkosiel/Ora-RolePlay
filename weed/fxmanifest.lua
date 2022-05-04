3c69da3f31d7241e3de3bd4f5268a8a9760a4f4b
Y0lac <ivelol@yahoo.fr>
2022-05-04 19:06:56 +0200

    weed

---
+++ weed/fxmanifest.lua
@@ -0,0 +1,26 @@
+exports "Ora"
+
+fx_version "adamant"
+
+game "gta5"
+
+files {
+	'ui/bg.png',
+	'ui/ferti-icone.png',
+	'ui/weed-icon.png',
+	'ui/water-icon.png',
+	'ui/index.html',
+	'ui/main.js',
+	'ui/main.css',
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'sv_main.lua'
+}
+
+client_script {
+	"cl_main.lua"
+}
+
+ui_page 'ui/index.html'
