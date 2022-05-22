4a9c14f0546addf48bf421ca92ca841c9d63b03f
Y0lac <ivelol@yahoo.fr>
2022-05-22 22:19:40 +0200

    Update config.lua

--- myDj/config.lua
+++ myDj/config.lua
@@ -53,6 +53,13 @@ Config.DJPositions = {
         requiredJob = nil, 
         range = 15.0, 
         volume = 1.0 --[[ do not touch the volume! --]]
+    },
+    {
+        name = 'runner',
+        pos = vector3(149.07, -3013.24, 7.04 - 0.98),
+        requiredJob = nil, 
+        range = 15.0, 
+        volume = 1.0 --[[ do not touch the volume! --]]
     }
 
     --{name = 'bahama', pos = vector3(-1381.01, -616.17, 31.5), requiredJob = 'DJ', range = 25.0}
