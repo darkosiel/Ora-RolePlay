f560431c65327db5f5a236c00b3057c1c575407f
Stealing <tom.dupont6470@gmail.com>
2022-05-31 13:08:25 +0200

    Update crouch.lua

--- Ora/utils/crouch.lua
+++ Ora/utils/crouch.lua
@@ -1,36 +1,31 @@
 local crouched = false
 
-Citizen.CreateThread(function()
-	while true do 
-		Citizen.Wait(1)
-		if IsControlPressed(0, 61) then DisableControlAction(0, 26, true) end
-		if (IsControlPressed(0, 26) or IsDisabledControlJustPressed(0, 26)) and IsControlPressed(0, 61) then
-			local ped = GetPlayerPed(-1)
-			if DoesEntityExist(ped) and not IsEntityDead(ped) then
-				RequestAnimSet("move_ped_crouched")
-				RequestAnimSet("MOVE_M@TOUGH_GUY@")
-				
-				while (not HasAnimSetLoaded("move_ped_crouched")) do 
-					Citizen.Wait(100)
-				end
+Citizen.CreateThread( function()
+    while true do 
+        Citizen.Wait( 1 )
 
-				while (not HasAnimSetLoaded("MOVE_M@TOUGH_GUY@")) do 
-					Citizen.Wait(100)
-				end
+        local ped = GetPlayerPed( -1 )
 
-				if crouched then 
-					ResetPedMovementClipset(ped, 0)
-					ResetPedStrafeClipset(ped)
-					SetPedMovementClipset(ped, "MOVE_M@TOUGH_GUY@", 0.5)
-					Wait(500)
-					ResetPedMovementClipset(ped, 0)
-					crouched = false 
-				elseif not crouched then
-					SetPedMovementClipset(ped, "move_ped_crouched", 0.55)
-					SetPedStrafeClipset(ped, "move_ped_crouched_strafing")
-					crouched = true 
-				end
-			end
-		end    
-	end
-end)
+        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+            DisableControlAction( 0, 36, true ) -- INPUT_DUCK  
+
+            if ( not IsPauseMenuActive() ) then 
+                if ( IsDisabledControlJustPressed( 0, 36 ) ) then 
+                    RequestAnimSet( "move_ped_crouched" )
+
+                    while ( not HasAnimSetLoaded( "move_ped_crouched" ) ) do 
+                        Citizen.Wait( 100 )
+                    end 
+
+                    if ( crouched == true ) then 
+                        ResetPedMovementClipset( ped, 0 )
+                        crouched = false 
+                    elseif ( crouched == false ) then
+                        SetPedMovementClipset( ped, "move_ped_crouched", 0.50 )
+                        crouched = true 
+                    end 
+                end
+            end 
+        end 
+    end
+end )
