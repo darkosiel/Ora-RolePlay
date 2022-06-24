d7620659d924fd27e90deea9dcc2d75f97523fb5
Zelda601 <tom.dupont6470@gmail.com>
2022-06-24 12:42:02 +0200

    Update config.lua

--- myDj/config.lua
+++ myDj/config.lua
@@ -62,6 +62,13 @@ Config.DJPositions = {
         volume = 1.0 --[[ do not touch the volume! --]]
     },
     {
+        name = 'Purl',
+        pos = vector3(-1831.1268, -1190.3728, 19.87 - 0.98),
+        requiredJob = nil, 
+        range = 15.0, 
+        volume = 1.0 --[[ do not touch the volume! --]]
+    },
+    {
         name = 'runner',
         pos = vector3(149.07, -3013.24, 7.04 - 0.98),
         requiredJob = nil, 
