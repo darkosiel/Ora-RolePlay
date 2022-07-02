6c4e4a003ba2b19dd0b9f40faf7e51387c04e84f
Zelda601 <tom.dupont6470@gmail.com>
2022-07-02 19:06:07 +0200

    Update cl_vehshop.lua

--- Ora/gameplay/shops/vehicle/cl_vehshop.lua
+++ Ora/gameplay/shops/vehicle/cl_vehshop.lua
@@ -394,7 +394,7 @@ local VehShop =
                     {name = "polraiden", price = 25000},
                     {name = "suppressor", price = 25000},
                     {name = "sebalamo2", price = 25000},
-                    {name = "prevolter", price =25000 1}
+                    {name = "prevolter", price = 25000}
                 },
                 ["Vans"] = {
                     {name = "minivan", price = 9100},
