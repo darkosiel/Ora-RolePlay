c077b4570417dadb1b0475026bffce86f1d1648c
Zelda601 <tom.dupont6470@gmail.com>
2022-07-12 19:02:59 +0200

    add véhicle

---
+++ [vehicle]/Ora_japmod/fxmanifest.lua
@@ -0,0 +1,240 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'shiiro'
+files {
+	'common/algoprimo3/algoprimo3-carcols.meta',
+	'common/algoprimo3/algoprimo3-carvariations.meta',
+	'common/algoprimo3/algoprimo3-dlctext.meta',
+	'common/algoprimo3/algoprimo3-handling.meta',
+	'common/algoprimo3/algoprimo3-vehicles.meta',
+	'common/algoprimo3/*.meta',
+
+	'common/ALTIOR/ALTIOR-carcols.meta',
+	'common/ALTIOR/ALTIOR-carvariations.meta',
+	'common/ALTIOR/ALTIOR-dlctext.meta',
+	'common/ALTIOR/ALTIOR-handling.meta',
+	'common/ALTIOR/ALTIOR-vehicles.meta',
+	'common/ALTIOR/*.meta',
+
+	'common/doubled/doubled-carcols.meta',
+	'common/doubled/doubled-carvariations.meta',
+	'common/doubled/doubled-dlctext.meta',
+	'common/doubled/doubled-handling.meta',
+	'common/doubled/doubled-vehicles.meta',
+	'common/doubled/*.meta',
+
+	'common/dubsta22/dubsta22-carcols.meta',
+	'common/dubsta22/dubsta22-carvariations.meta',
+	'common/dubsta22/dubsta22-dlctext.meta',
+	'common/dubsta22/dubsta22-handling.meta',
+	'common/dubsta22/dubsta22-vehicles.meta',
+	'common/dubsta22/*.meta',
+
+	'common/dubstaenus/dubstaenus-carcols.meta',
+	'common/dubstaenus/dubstaenus-carvariations.meta',
+	'common/dubstaenus/dubstaenus-dlctext.meta',
+	'common/dubstaenus/dubstaenus-handling.meta',
+	'common/dubstaenus/dubstaenus-vehicles.meta',
+	'common/dubstaenus/*.meta',
+
+	'common/elegyrh7/elegyrh7-carcols.meta',
+	'common/elegyrh7/elegyrh7-carvariations.meta',
+	'common/elegyrh7/elegyrh7-dlctext.meta',
+	'common/elegyrh7/elegyrh7-handling.meta',
+	'common/elegyrh7/elegyrh7-vehicles.meta',
+	'common/elegyrh7/*.meta',
+
+	'common/gauntlets/gauntlets-carcols.meta',
+	'common/gauntlets/gauntlets-carvariations.meta',
+	'common/gauntlets/gauntlets-dlctext.meta',
+	'common/gauntlets/gauntlets-handling.meta',
+	'common/gauntlets/gauntlets-vehicles.meta',
+	'common/gauntlets/*.meta',
+
+	'common/nation_roxanne/nation_roxanne-carcols.meta',
+	'common/nation_roxanne/nation_roxanne-carvariations.meta',
+	'common/nation_roxanne/nation_roxanne-dlctext.meta',
+	'common/nation_roxanne/nation_roxanne-handling.meta',
+	'common/nation_roxanne/nation_roxanne-vehicles.meta',
+	'common/nation_roxanne/*.meta',
+
+	'common/oraclestd/oraclestd-carcols.meta',
+	'common/oraclestd/oraclestd-carvariations.meta',
+	'common/oraclestd/oraclestd-dlctext.meta',
+	'common/oraclestd/oraclestd-handling.meta',
+	'common/oraclestd/oraclestd-vehicles.meta',
+	'common/oraclestd/*.meta',
+
+	'common/SCHEISSER/SCHEISSER-carcols.meta',
+	'common/SCHEISSER/SCHEISSER-carvariations.meta',
+	'common/SCHEISSER/SCHEISSER-dlctext.meta',
+	'common/SCHEISSER/SCHEISSER-handling.meta',
+	'common/SCHEISSER/SCHEISSER-vehicles.meta',
+	'common/SCHEISSER/SCHEISSER-caraddoncontentunlocks.meta',
+	'common/SCHEISSER/*.meta',
+
+	'common/sg3_pack/sg3_pack-carcols.meta',
+	'common/sg3_pack/sg3_pack-carvariations.meta',
+	'common/sg3_pack/sg3_pack-dlctext.meta',
+	'common/sg3_pack/sg3_pack-handling.meta',
+	'common/sg3_pack/sg3_pack-vehicles.meta',
+	'common/sg3_pack/*.meta',
+
+	'common/sigma2/sigma2-carcols.meta',
+	'common/sigma2/sigma2-carvariations.meta',
+	'common/sigma2/sigma2-dlctext.meta',
+	'common/sigma2/sigma2-handling.meta',
+	'common/sigma2/sigma2-vehicles.meta',
+	'common/sigma2/*.meta',
+
+	'common/sppatriots/sppatriots-carcols.meta',
+	'common/sppatriots/sppatriots-carvariations.meta',
+	'common/sppatriots/sppatriots-dlctext.meta',
+	'common/sppatriots/sppatriots-handling.meta',
+	'common/sppatriots/sppatriots-vehicles.meta',
+	'common/sppatriots/*.meta',
+
+	'common/ssc_ellie/ssc_ellie-carcols.meta',
+	'common/ssc_ellie/ssc_ellie-carvariations.meta',
+	'common/ssc_ellie/ssc_ellie-dlctext.meta',
+	'common/ssc_ellie/ssc_ellie-handling.meta',
+	'common/ssc_ellie/ssc_ellie-vehicles.meta',
+	'common/ssc_ellie/*.meta',
+
+	'common/stratumc/stratumc-carcols.meta',
+	'common/stratumc/stratumc-carvariations.meta',
+	'common/stratumc/stratumc-dlctext.meta',
+	'common/stratumc/stratumc-handling.meta',
+	'common/stratumc/stratumc-vehicles.meta',
+	'common/stratumc/*.meta',
+
+	'common/sunriser/sunriser-carcols.meta',
+	'common/sunriser/sunriser-carvariations.meta',
+	'common/sunriser/sunriser-dlctext.meta',
+	'common/sunriser/sunriser-handling.meta',
+	'common/sunriser/sunriser-vehicles.meta',
+	'common/sunriser/*.meta',
+
+	'common/tampar/tampar-carcols.meta',
+	'common/tampar/tampar-carvariations.meta',
+	'common/tampar/tampar-dlctext.meta',
+	'common/tampar/tampar-handling.meta',
+	'common/tampar/tampar-vehicles.meta',
+	'common/tampar/*.meta',
+
+	'common/VTBUFFALOAC/VTBUFFALOAC-carcols.meta',
+	'common/VTBUFFALOAC/VTBUFFALOAC-carvariations.meta',
+	'common/VTBUFFALOAC/VTBUFFALOAC-dlctext.meta',
+	'common/VTBUFFALOAC/VTBUFFALOAC-handling.meta',
+	'common/VTBUFFALOAC/VTBUFFALOAC-vehicles.meta',
+	'common/VTBUFFALOAC/*.meta'
+}
+
+data_file 'DLCTEXT_FILE' 'common/algoprimo3/algoprimo3-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/algoprimo3/algoprimo3-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/algoprimo3/algoprimo3-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/algoprimo3/algoprimo3-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/algoprimo3/algoprimo3-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/ALTIOR/ALTIOR-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/ALTIOR/ALTIOR-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/ALTIOR/ALTIOR-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/ALTIOR/ALTIOR-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/ALTIOR/ALTIOR-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/doubled/doubled-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/doubled/doubled-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/doubled/doubled-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/doubled/doubled-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/doubled/doubled-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/dubsta22/dubsta22-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/dubsta22/dubsta22-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/dubsta22/dubsta22-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/dubsta22/dubsta22-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/dubsta22/dubsta22-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/dubstaenus/dubstaenus-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/dubstaenus/dubstaenus-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/dubstaenus/dubstaenus-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/dubstaenus/dubstaenus-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/dubstaenus/dubstaenus-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/elegyrh7/elegyrh7-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/elegyrh7/elegyrh7-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/elegyrh7/elegyrh7-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/elegyrh7/elegyrh7-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/elegyrh7/elegyrh7-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/gauntlets/gauntlets-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/gauntlets/gauntlets-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/gauntlets/gauntlets-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/gauntlets/gauntlets-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/gauntlets/gauntlets-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/nation_roxanne/nation_roxanne-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/nation_roxanne/nation_roxanne-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/nation_roxanne/nation_roxanne-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/nation_roxanne/nation_roxanne-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/nation_roxanne/nation_roxanne-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/oraclestd/oraclestd-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/oraclestd/oraclestd-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/oraclestd/oraclestd-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/oraclestd/oraclestd-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/oraclestd/oraclestd-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/SCHEISSER/SCHEISSER-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/SCHEISSER/SCHEISSER-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/SCHEISSER/SCHEISSER-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/SCHEISSER/SCHEISSER-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/SCHEISSER/SCHEISSER-carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'common/SCHEISSER/SCHEISSER-caraddoncontentunlocks.meta'
+
+data_file 'DLCTEXT_FILE' 'common/sg3_pack/sg3_pack-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/sg3_pack/sg3_pack-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/sg3_pack/sg3_pack-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/sg3_pack/sg3_pack-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/sg3_pack/sg3_pack-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/sigma2/sigma2-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/sigma2/sigma2-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/sigma2/sigma2-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/sigma2/sigma2-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/sigma2/sigma2-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/sppatriots/sppatriots-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/sppatriots/sppatriots-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/sppatriots/sppatriots-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/sppatriots/sppatriots-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/sppatriots/sppatriots-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/ssc_ellie/ssc_ellie-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/ssc_ellie/ssc_ellie-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/ssc_ellie/ssc_ellie-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/ssc_ellie/ssc_ellie-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/ssc_ellie/ssc_ellie-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/stratumc/stratumc-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/stratumc/stratumc-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/stratumc/stratumc-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/stratumc/stratumc-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/stratumc/stratumc-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/sunriser/sunriser-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/sunriser/sunriser-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/sunriser/sunriser-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/sunriser/sunriser-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/sunriser/sunriser-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/tampar/tampar-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/tampar/tampar-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/tampar/tampar-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/tampar/tampar-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/tampar/tampar-carvariations.meta'
+
+data_file 'DLCTEXT_FILE' 'common/VTBUFFALOAC/VTBUFFALOAC-dlctext.meta'
+data_file 'HANDLING_FILE' 'common/VTBUFFALOAC/VTBUFFALOAC-handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/VTBUFFALOAC/VTBUFFALOAC-vehicles.meta'
+data_file 'CARCOLS_FILE' 'common/VTBUFFALOAC/VTBUFFALOAC-carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'common/VTBUFFALOAC/VTBUFFALOAC-carvariations.meta'
