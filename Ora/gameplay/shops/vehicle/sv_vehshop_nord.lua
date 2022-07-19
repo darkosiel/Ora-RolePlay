195464af3809d26dff4c8045a6a1260089741e5c
Zelda601 <tom.dupont6470@gmail.com>
2022-07-19 11:36:17 +0200

    Update sv_vehshop_nord.lua

--- Ora/gameplay/shops/vehicle/sv_vehshop_nord.lua
+++ Ora/gameplay/shops/vehicle/sv_vehshop_nord.lua
@@ -1,3 +1,45 @@
+--[[RegisterServerCallback("autoshop:BuyVehicle", function(source, callback,price,targetSource,vehicle)
+    local _veh = json.encode(vehicle)
+    local pUUID = Player.GetPlayer(source).uuid
+    local uuid = Player.GetPlayer(targetSource).uuid
+
+    MySQL.Async.execute(
+        'INSERT INTO players_vehicles (uuid,settings,plate,label) VALUES(@uuid,@settings,@plate,@label)',
+        {
+            ['@uuid'] = uuid,
+            ['@settings'] = _veh,
+            ['@plate'] = vehicle.plate,
+            ['@label'] = vehicle.label,
+
+        }
+    )
+
+    MySQL.Async.execute(
+        'INSERT INTO vehicles_sold_nord (buyer,model,plate,vendor,prices,date) VALUES(@buyer,@model,@plate,@vendor,@price,@date)',
+        {
+            ['@buyer'] = uuid,
+            ['@vendor'] = pUUID,
+            ['@plate'] = vehicle.plate,
+            ['@model'] = vehicle.label,
+            ['@price'] = price,
+            ['@date'] = os.date("%Y-%m-%d %H:%M:%S", os.time())
+
+        }
+    )
+    --(true)
+    callback(true)
+end)
+
+RegisterServerCallback("autoshop:GetAll", function(source, callback)
+
+    MySQL.Async.fetchAll('SELECT * FROM vehicles_sold_nord  ', {}, function(result)
+        MySQL.Async.fetchAll('SELECT * FROM players_identity', {}, function(_result)
+            callback(result,_result)
+        end)
+    end)
+end]]
+-----------------------------------------------------
+
 local function insertNewVehicleIntoDatabase(seller, buyer, vehicleOptions, plate, label, price)
     local vehicleOptionsObject = json.decode(vehicleOptions)
     MySQL.Async.execute(
@@ -33,7 +75,7 @@ RegisterServerCallback(
         while (Ora.Utils:HasValue(Ora.World.Vehicle.ExistingPlates, vehicle.plate)) do
             vehicle.plate = Ora.World.Vehicle:GenerateRandomPlate()
         end
-        insertNewVehicleIntoDatabase(seller, "jetsam", vehicleOptions, vehicle.plate, vehicle.label, price)
+        insertNewVehicleIntoDatabase(seller, "concess", vehicleOptions, vehicle.plate, vehicle.label, price)
         callback(true, vehicle.plate)
     end
 )
@@ -46,7 +88,7 @@ RegisterServerCallback(
         while (Ora.Utils:HasValue(Ora.World.Vehicle.ExistingPlates, vehicle.plate)) do
             vehicle.plate = Ora.World.Vehicle:GenerateRandomPlate()
         end
-        insertNewVehicleIntoDatabase(seller, company, vehicleOptions, vehicle.plate, vehicle.label, price)
+        insertNewVehicleIntoDatabase(seller, "concess", vehicleOptions, vehicle.plate, vehicle.label, price)
         callback(true, vehicle.plate)
     end
 )
