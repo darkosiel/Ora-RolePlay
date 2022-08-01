c935a2697c9d4a3d143fb56ec5a732fec5980e63
Shiiro7 <extazznico@outlook.fr>
2022-08-01 06:12:25 +0200

    update blip

--- Ora/utils/cl_blips.lua
+++ Ora/utils/cl_blips.lua
@@ -97,7 +97,7 @@ local Blips = {
    -- {name = "Fujiwara Tofu", color = 63, sprite = 210, size = 1.0, Pos = {x = -165.19, y = 290.42, z = 93.76}},
     {name = "Car Wash", color = 27, sprite = 100, Pos = {x = 169.58, y = -1718.31, z = 29.29}},
     {name = "Casino", color = 26, sprite = 89, Pos = {x = 937.8317, y = 32.8164, z = 112.4599}},
-    {name = "Bar - San-Inn", color = 59, sprite = 93, Pos = {x = -820.6145, y = -697.8694, z = 112.4599}},
+    {name = "Bar - San-Inn", color = 59, sprite = 93, Pos = {x = -172.66, y = 291.57, z = 92.76}},
     --{name = "Armurerie - Paleto Bay", color = 49, sprite = 313, Pos = {x = -326.78, y = 6080.18, z = 31.45}}
     {name = "Death Row Records", color = 49, sprite = 355, Pos = {x = -1441.7468, y = -546.6607, z = 34.7418}}
 }
