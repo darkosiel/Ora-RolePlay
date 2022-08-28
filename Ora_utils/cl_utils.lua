cea5fa6a302abde4af615d4d5c8aa6c17e833300
root <root@A7802D7>
2022-08-28 19:57:09 +0000

    try to optimize thread by avoiding calling same natives several times in a thread, which is useless

--- Ora_utils/cl_utils.lua
+++ Ora_utils/cl_utils.lua
@@ -65,9 +65,15 @@ local vehicleGenerator = {
 }
 
 
+Player = {
+    Ped = PlayerPedId(),
+    Position = vector3(0, 0, 0),
+    PlayerId = PlayerId(),
+}
+
 
 local function startPointing()
-    local ped = PlayerPedId()
+    local ped = Player.Ped
     RequestAnimDict("anim@mp_point")
     while not HasAnimDictLoaded("anim@mp_point") do
         Wait(50)
@@ -79,7 +85,7 @@ local function startPointing()
 end
 
 local function stopPointing()
-    local ped = PlayerPedId()
+    local ped = Player.Ped
     Citizen.InvokeNative(0xD01015C7316AE176, ped, "Stop")
     if not IsPedInjured(ped) then
         ClearPedSecondaryTask(ped)
@@ -88,11 +94,11 @@ local function stopPointing()
         SetPedCurrentWeaponVisible(ped, 1, 1, 1, 1)
     end
     SetPedConfigFlag(ped, 36, 0)
-    ClearPedSecondaryTask(PlayerPedId())
+    ClearPedSecondaryTask(Player.Ped)
 end
 local pT = false
 function pointKey()
-    if IsPedOnFoot(PlayerPedId()) then
+    if IsPedOnFoot(Player.Ped) then
         if not pT then
             startPointing()
         else
@@ -138,7 +144,7 @@ Citizen.CreateThread(
             end
             if ishandcuff == false then
                 if not keyPressed then
-                    if IsControlPressed(0, 29) and not mp_pointing and IsPedOnFoot(PlayerPedId()) then
+                    if IsControlPressed(0, 29) and not mp_pointing and IsPedOnFoot(Player.Ped) then
                         Wait(200)
                         if not IsControlPressed(0, 29) then
                             keyPressed = true
@@ -150,7 +156,7 @@ Citizen.CreateThread(
                                 Wait(50)
                             end
                         end
-                    elseif (IsControlPressed(0, 29) and mp_pointing) or (not IsPedOnFoot(PlayerPedId()) and mp_pointing) then
+                    elseif (IsControlPressed(0, 29) and mp_pointing) or (not IsPedOnFoot(Player.Ped) and mp_pointing) then
                         keyPressed = true
                         mp_pointing = false
                     --stopPointing()
@@ -162,14 +168,14 @@ Citizen.CreateThread(
                         keyPressed = false
                     end
                 end
-                if Citizen.InvokeNative(0x921CE12C489C4C41, PlayerPedId()) and not mp_pointing then
+                if Citizen.InvokeNative(0x921CE12C489C4C41, Player.Ped) and not mp_pointing then
                     stopPointing()
                 end
-                if Citizen.InvokeNative(0x921CE12C489C4C41, PlayerPedId()) then
-                    if not IsPedOnFoot(PlayerPedId()) then
+                if Citizen.InvokeNative(0x921CE12C489C4C41, Player.Ped) then
+                    if not IsPedOnFoot(Player.Ped) then
                         stopPointing()
                     else
-                        local ped = PlayerPedId()
+                        local ped = Player.Ped
                         local camPitch = GetGameplayCamRelativePitch()
                         if camPitch < -70.0 then
                             camPitch = -70.0
@@ -229,74 +235,93 @@ Citizen.CreateThread(
     end
 )
 
-local clearAreaOfPedsArray = { --Antispawn PNJ
-    { pos = vector3(-1084.15, -821.88, 5.48), radius = 60.0 },
-    { pos = vector3(326.78, -583.25, 43.4), radius = 50.0 },
-    { pos = vector3(-556.62, 285.77, 82.18), radius = 25.0 },
-    { pos = vector3(338.99, 287.44, 99.80), radius = 25.0 },
-    { pos = vector3(-1572.96, -261.3, 48.48), radius = 50.0 },
-    { pos = vector3(-580.09, -932.8, 23.68), radius = 25.0 },
-    { pos = vector3(-1070.56, -245.15, 40.35), radius = 25.0 },
-    { pos = vector3(-559.68, -199.2, 39.34), radius = 25.0 },
-    { pos = vector3(245.51, -1093.77, 30.21), radius = 25.0 },
-    { pos = vector3(117.96, -1287.3, 28.27), radius = 50.0 },
-    { pos = vector3(322.58, 181.54, 103.59), radius = 6.0 },
-    { pos = vector3(248.62, 220.36, 106.29), radius = 15.0 },
-    { pos = vector3(-204.8388, -1333.2086, 34.89), radius = 60.0 },
-    { pos = vector3(-1427.299, -245.1012, 16.8039), radius = 20.0 },
-    { pos = vector3(1990.88, 3054.02, 47.21), radius = 60.0 },-- Yellow Jack 
-    { pos = vector3(811.8556, -2149.9516, 29.6210), radius = 60.0 },-- Ammunation Sud
-    { pos = vector3(1410.99, 1147.39, 114.33), radius = 60.0 },-- Madrazo
-    { pos = vector3(146.24, -1113.88, 29.31), radius = 40.0 },-- PDM
-    { pos = vector3(-1839.58, -360.67, 59.00), radius = 40.0 },-- Hopital
-}
+-- local clearAreaOfPedsArray = { --Antispawn PNJ
+--     { pos = vector3(-1084.15, -821.88, 5.48), radius = 60.0 },
+--     { pos = vector3(326.78, -583.25, 43.4), radius = 50.0 },
+--     { pos = vector3(-556.62, 285.77, 82.18), radius = 25.0 },
+--     { pos = vector3(338.99, 287.44, 99.80), radius = 25.0 },
+--     { pos = vector3(-1572.96, -261.3, 48.48), radius = 50.0 },
+--     { pos = vector3(-580.09, -932.8, 23.68), radius = 25.0 },
+--     { pos = vector3(-1070.56, -245.15, 40.35), radius = 25.0 },
+--     { pos = vector3(-559.68, -199.2, 39.34), radius = 25.0 },
+--     { pos = vector3(245.51, -1093.77, 30.21), radius = 25.0 },
+--     { pos = vector3(117.96, -1287.3, 28.27), radius = 50.0 },
+--     { pos = vector3(322.58, 181.54, 103.59), radius = 6.0 },
+--     { pos = vector3(248.62, 220.36, 106.29), radius = 15.0 },
+--     { pos = vector3(-204.8388, -1333.2086, 34.89), radius = 60.0 },
+--     { pos = vector3(-1427.299, -245.1012, 16.8039), radius = 20.0 },
+--     { pos = vector3(1990.88, 3054.02, 47.21), radius = 60.0 },-- Yellow Jack 
+--     { pos = vector3(811.8556, -2149.9516, 29.6210), radius = 60.0 },-- Ammunation Sud
+--     { pos = vector3(1410.99, 1147.39, 114.33), radius = 60.0 },-- Madrazo
+--     { pos = vector3(146.24, -1113.88, 29.31), radius = 40.0 },-- PDM
+--     { pos = vector3(-1839.58, -360.67, 59.00), radius = 40.0 },-- Hopital
+-- }
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1000)
-        local playerPosition = GetEntityCoords(PlayerPedId()) 
-        ClearAreaOfCops(playerPosition.x, playerPosition.y, playerPosition.z, 500.0, 0)
-    end
-end)
+-- Citizen.CreateThread(function()
+--     local PlayerId, PlayerPedId, GetEntityCoords, ClearAreaOfCops = PlayerId, PlayerPedId, GetEntityCoords, ClearAreaOfCops
+--     while true do
+--         Citizen.Wait(1000)
+--         Player.PlayerId = PlayerId()
+--         Player.Ped = PlayerPedId()
+--         Player.Position = GetEntityCoords(Player.Ped)
+--         ClearAreaOfCops(Player.Position.x, Player.Position.y, Player.Position.z, 500.0, 0)
+--     end
+-- end)
 
-Citizen.CreateThread(
-    function()
-        SetPoliceIgnorePlayer(PlayerId(), true)
-        SetEveryoneIgnorePlayer(PlayerId(), true)
-        SetPlayerCanBeHassledByGangs(PlayerId(), false)
-        SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_HILLBILLY"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_BALLAS"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MEXICAN"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_FAMILY"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MARABUNTE"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_SALVA"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("GANG_1"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("GANG_2"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("GANG_9"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("GANG_10"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("FIREMAN"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("MEDIC"), GetHashKey('PLAYER'))
-        SetRelationshipBetweenGroups(1, GetHashKey("COP"), GetHashKey('PLAYER'))
+-- Citizen.CreateThread(function()
+--         local GetHashKey = GetHashKey
+--         local ClearAreaOfPeds = ClearAreaOfPeds
+--         local SetPedDensityMultiplierThisFrame = SetPedDensityMultiplierThisFrame
+--         local SetScenarioPedDensityMultiplierThisFrame = SetScenarioPedDensityMultiplierThisFrame
+--         local SetVehicleDensityMultiplierThisFrame = SetVehicleDensityMultiplierThisFrame
+--         local SetParkedVehicleDensityMultiplierThisFrame = SetParkedVehicleDensityMultiplierThisFrame
+--         local SetRandomVehicleDensityMultiplierThisFrame = SetRandomVehicleDensityMultiplierThisFrame
+--         local DisablePlayerVehicleRewards = DisablePlayerVehicleRewards
+--         local SetPlayerHealthRechargeMultiplier = SetPlayerHealthRechargeMultiplier
+--         SetPoliceIgnorePlayer(Player.PlayerId, true)
+--         SetEveryoneIgnorePlayer(Player.PlayerId, true)
+--         SetPlayerCanBeHassledByGangs(Player.PlayerId, false)
+--         SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_HILLBILLY"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_BALLAS"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MEXICAN"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_FAMILY"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_MARABUNTE"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("AMBIENT_GANG_SALVA"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("GANG_1"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("GANG_2"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("GANG_9"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("GANG_10"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("FIREMAN"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("MEDIC"), GetHashKey('PLAYER'))
+--         SetRelationshipBetweenGroups(1, GetHashKey("COP"), GetHashKey('PLAYER'))
+--         while true do
+--             Citizen.Wait(0)
 
-        while true do
-            Citizen.Wait(0)
+--             -- for key, value in pairs(clearAreaOfPedsArray) do
+--             --     if #(value.pos - Player.Position) < 250.0 then
+--             --         print("Deleting peds for " .. key)
+--             --         ClearAreaOfPeds(value.pos.x, value.pos.y, value.pos.z, value.radius, 1)
+--             --     end
+--             -- end
+--             SetPedDensityMultiplierThisFrame(1.0) -- set npc/ai peds density to 0
+--             SetScenarioPedDensityMultiplierThisFrame(1.0, 1.0)
+--             SetVehicleDensityMultiplierThisFrame(0.8) -- was at 0.6
+--             SetParkedVehicleDensityMultiplierThisFrame(0.8) -- was at 0.5
+--             SetRandomVehicleDensityMultiplierThisFrame(0.8) -- was at 0.5 -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
+--             DisablePlayerVehicleRewards(Player.PlayerId)
+--             SetPlayerHealthRechargeMultiplier(Player.PlayerId, 0.0)
+--         end
+--     end
+-- )
 
-            local playerPosition = GetEntityCoords(PlayerPedId()) 
-            for key, value in pairs(clearAreaOfPedsArray) do
-                if (GetDistanceBetweenCoords(value.pos, playerPosition, false) < 250.0) then
-                    ClearAreaOfPeds(value.pos.x, value.pos.y, value.pos.z, value.radius, 1)
-                end
-            end
-            SetPedDensityMultiplierThisFrame(1.0) -- set npc/ai peds density to 0
-            SetScenarioPedDensityMultiplierThisFrame(1.0, 1.0)
-            SetVehicleDensityMultiplierThisFrame(0.8) -- was at 0.6
-            SetParkedVehicleDensityMultiplierThisFrame(0.8) -- was at 0.5
-            SetRandomVehicleDensityMultiplierThisFrame(0.8) -- was at 0.5 -- set random vehicles (car scenarios / cars driving off from a parking spot etc.) to 0
-            DisablePlayerVehicleRewards(PlayerId())
-            SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
-        end
-    end
-)
+-- AddEventHandler("populationPedCreating", function(x, y, z, model, functions)
+--     for k, v in pairs(ClearAreaOfPedsArray) do
+--         if #(v.pos - vector3(x, y, z)) < v.radius then
+--             print("Cancelling ped creation for " .. k)
+--             CancelEvent()
+--         end
+--     end
+-- end)
 
 local function CheckResolution(firstConn)
     local x, y = GetActiveScreenResolution()
@@ -403,8 +428,8 @@ end
             HideHudComponentThisFrame(141)
             HideHudComponentThisFrame(2)
             if not ishandcuff then
-            -- if IsControlPressed(1, 323) and IsPedOnFoot(PlayerPedId())  then
-            -- 	local ped = PlayerPedId()
+            -- if IsControlPressed(1, 323) and IsPedOnFoot(Player.Ped)  then
+            -- 	local ped = Player.Ped
 
             -- 	if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then
 
@@ -460,7 +485,7 @@ Citizen.CreateThread(
     function()
         while true do
             Wait(0)
-            local ped = PlayerPedId()
+            local ped = Player.Ped
 
             if IsPedArmed(ped, 6) then
                 DisableControlAction(1, 140, true)
@@ -470,7 +495,7 @@ Citizen.CreateThread(
                     DisableControlAction(1, 25, true)
                     DisableControlAction(1, 68, true)
                 end
-                if IsPlayerFreeAiming(PlayerId()) then
+                if IsPlayerFreeAiming(Player.PlayerId) then
                     DisableControlAction(1, 22, true)
                     DisableControlAction(1, 36, true)
                 end
@@ -487,8 +512,7 @@ AddEventHandler(
     function()
         Citizen.CreateThread(
             function()
-                local player = PlayerId()
-                local playerPed = PlayerPedId()
+                local playerPed = Player.Ped
 
                 NetworkSetFriendlyFireOption(true)
                 SetCanAttackFriendly(playerPed, true, true)
