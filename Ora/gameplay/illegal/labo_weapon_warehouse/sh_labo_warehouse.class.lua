70fce0e51ba35c4112343ff160d526f278d33665
Shiiro7 <extazznico@outlook.fr>
2022-09-24 19:15:15 +0200

    modif weapons gl

--- Ora/gameplay/illegal/labo_weapon_warehouse/sh_labo_warehouse.class.lua
+++ Ora/gameplay/illegal/labo_weapon_warehouse/sh_labo_warehouse.class.lua
@@ -679,7 +679,7 @@ IllegalLabsAndWarehouse.AVAILABLE_GUNLEADS = {
       {name = "shootguncompact", byWeek = 0, price = 50000},
       {name = "revolver", byWeek = 0, price = 14000},
       {name = "pistol50", byWeek = 0, price = 16000},
-      {name = "dbshotgun", byWeek = 0, price = 45000},
+      {name = "dbshotgun", byWeek = 0, price = 70000},
       {name = "ceramicpistol", byWeek = 0, price = 12000},
       {name = "minismg", byWeek = 0, price = 35000},
       {name = "machinepistol", byWeek = 0, price = 65000},
@@ -791,7 +791,7 @@ IllegalLabsAndWarehouse.AVAILABLE_GUNLEADS = {
       -- This is not cumulative, so you always have to start from the base limitation
       [6] = {
           label = "Confiance commerciale 6",
-          description = "Pour 85 000$ vous obtenez 10 Berettas, 10 Pistolet Céramique, 5 pistolets lourds, 2 Skorpions, 2500 balles de 9mm, 2 Tec9, 1 Gusenberg, 2 Uzis 60 Calibre 12 et 20 explosifs artisanaux en plus par semaine par rapport à la limite de base",
+          description = "Pour 85 000$ vous obtenez 10 Berettas, 10 Pistolet Céramique, 3 Calibre 50, 5 pistolets lourds, 2 Skorpions, 2500 balles de 9mm, 2 Tec9, 1 Gusenberg, 2 Uzis 60 Calibre 12 et 20 explosifs artisanaux en plus par semaine par rapport à la limite de base",
           price = 85000,
           addon = {
             pistol = 33,
@@ -801,7 +801,7 @@ IllegalLabsAndWarehouse.AVAILABLE_GUNLEADS = {
             dbshotgun = 4,
             calibre12 = 330,
             revolver = 2,
-            pistol50 = 2,
+            pistol50 = 5,
             minismg = 7,
             machinepistol = 5,
             microsmg = 2,
