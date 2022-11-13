19a6e31f0428fdd79509868dfaf023463e4d6a50
PaoloOra <dastara33@gmail.com>
2022-11-13 18:35:35 +0100

    maj map ora phone

--- [others]/OraPhone/html/script.js
+++ [others]/OraPhone/html/script.js
@@ -4483,6 +4483,8 @@ class ContextMenu {
 const Blips = [
     {name: "Poste de police", color: 29, sprite: 60, size: 1.0, Pos: {x: -1072.52, y: -856.42, z: 4.87}},
     {name: "Sherif - Paleto Bay", color: 70, sprite: 60, size: 1.0, Pos: {x: -440.43, y: 6019.48, z: 31.49}},
+    {name: "Maze Group", color: 69, sprite: 67, size: 1.0, Pos: {x: -1376.18, y: -491.93, z: 33.16}},
+    {name: "Gruppe Sechs", color: 49, sprite: 78, size: 1.0, Pos: {x: -231.46, y: -851.80, z: 30.68}},
     {name: "Vanilla Unicorn", color: 61, sprite: 121, size: 0.8, Pos: {x: 119.47, y: -1308.58, z: 29.71}},
     {name: "Distributeur de Billets", color: 2, sprite: 434, size: 0.80, Pos: {x: -526.065, y: -1221.99, z: 18.4549}},
     {name: "Distributeur de Billets", color: 2, sprite: 434, size: 0.80, Pos: {x: -2073.44, y: -317.245, z: 13.316}},
