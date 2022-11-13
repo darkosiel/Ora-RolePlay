4dd9b70a89cf7a179cee865748363cf180717c50
PaoloOra <dastara33@gmail.com>
2022-11-13 19:21:42 +0100

    pos fourrière benny's

--- Ora/statics/assets/Jobs.lua
+++ Ora/statics/assets/Jobs.lua
@@ -10467,7 +10467,7 @@ Jobs = {
         Extrapos = {
             Tow = {
                 Pos = {
-                    {x = -1052.71, y = -858.48, z = 4.88}
+                    {x = -162.45, y = -1304.8, z = 30.31}
                 },
                 Enter = EnterZoneTow,
                 Exit = ExitZoneTow,
