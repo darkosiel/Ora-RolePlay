e195ea5129057704a7b12b5ed100efb369a3c94f
Zelda601 <tom.dupont6470@gmail.com>
2022-12-04 15:52:02 +0100

    fix brancards

--- Ora/ui/menus/jobs/ambulance.lua
+++ Ora/ui/menus/jobs/ambulance.lua
@@ -200,7 +200,7 @@ function Ambulance.PutStretcherOnVehicle()
     local stretcher = GetClosestObjectOfType(coords, 3.0, stretcherHash)
     if stretcher and GetEntityAttachedTo(stretcher) == ped then
         local veh = GetVehicleInDirection()
-        if veh ~= 0 and GetEntityModel(veh) == GetHashKey("emsnspeedo") then
+        if veh ~= 0 and GetEntityModel(veh) == GetHashKey("samsbisonamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb2") or GetEntityModel(veh) == GetHashKey("Romero") or GetEntityModel(veh) == GetHashKey("medevac") then
             DetachEntity(stretcher)
             AttachEntityToEntity(stretcher, veh, 0.0, 0.0, -2.0, 0.8, 0.0, 0.0, 0.0, false, false, false, false, 2, true)
             FreezeEntityPosition(stretcher, true)
