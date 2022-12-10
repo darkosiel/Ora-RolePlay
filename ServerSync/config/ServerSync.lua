f854a87feb3c06af306ba58e42bac451a2d4f2e6
Zelda601 <tom.dupont6470@gmail.com>
2022-12-11 00:00:28 +0100

    cycle jour/nuit plus longs

--- ServerSync/config/ServerSync.lua
+++ ServerSync/config/ServerSync.lua
@@ -113,5 +113,5 @@ ss_wind_speed_Mult = {
 --#####################################################################################################################
 
 -- Lower then 1 = Longer, Higher then 1 = faster.  0.25 would be 4x slower then GTA time. 2.0 would be half as long as default GTA
-ss_night_time_speed_mult = 1.0
-ss_day_time_speed_mult = 1.0
+ss_night_time_speed_mult = 2.0
+ss_day_time_speed_mult = 2.0
