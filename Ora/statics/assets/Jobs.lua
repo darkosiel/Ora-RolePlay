50c9c8f2247f893fc2df1fa23c0d386903a0b14f
PaoloOra <dastara33@gmail.com>
2023-01-04 18:38:58 +0100

    fix

--- Ora/statics/assets/Jobs.lua
+++ Ora/statics/assets/Jobs.lua
@@ -10773,7 +10773,7 @@ Jobs = {
         Extrapos = {
             Tow = {
                 Pos = {
-                    {x = -162.45, y = -1304.8, z = 30.31}
+                    {x = 412.60, y = -987.57, z = 23.32}
                 },
                 Enter = EnterZoneTow,
                 Exit = ExitZoneTow,
