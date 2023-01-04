a7c0c8ec3bf47e54fce596f26a18cbd11b721b1d
Y0lac <ivelol@yahoo.fr>
2023-01-04 18:04:37 +0100

    mrpd doors

--- [others]/sc-doors/config.lua
+++ [others]/sc-doors/config.lua
@@ -10,130 +10,153 @@ Config.DoorList = {
     --
 
     -- Entrance Doors
-   --[[  {
-        textCoords = vector3(434.7, -982.0, 31.5),
-        authorizedJobs = {"police", "lssd"},
-        locked = false,
-        maxDistance = 2.5,
-        doors = {
-            {objHash = GetHashKey("v_ilev_ph_door01"), objHeading = 270.0, objCoords = vector3(434.7, -980.6, 30.8)},
-            {objHash = GetHashKey("v_ilev_ph_door002"), objHeading = 270.0, objCoords = vector3(434.7, -983.2, 30.8)}
-        }
-    },
-    -- To locker room & roof
-    {
-        objHash = GetHashKey("v_ilev_ph_gendoor004"),
-        objHeading = 90.0,
-        objCoords = vector3(449.6, -986.4, 30.6),
-        textCoords = vector3(450.1, -986.3, 31.7),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
-    },
-    -- Rooftop
-    {
-        objHash = GetHashKey("v_ilev_gtdoor02"),
-        objHeading = 90.0,
-        objCoords = vector3(464.3, -984.6, 43.8),
-        textCoords = vector3(464.3, -984.0, 44.8),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
-    },
-    -- Hallway to roof
-    {
-        objHash = GetHashKey("v_ilev_arm_secdoor"),
-        objHeading = 90.0,
-        objCoords = vector3(461.2, -985.3, 30.8),
-        textCoords = vector3(461.5, -986.0, 31.5),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
-    },
-    -- Armory
-    {
-        objHash = GetHashKey("v_ilev_arm_secdoor"),
-        objHeading = 270.0,
-        objCoords = vector3(452.6, -982.7, 30.6),
-        textCoords = vector3(453.0, -982.6, 31.7),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
-    },
-    -- Captain Office
-    {
-        objHash = GetHashKey("v_ilev_ph_gendoor002"),
-        objHeading = 180.0,
-        objCoords = vector3(447.2, -980.6, 30.6),
-        textCoords = vector3(447.2, -980.0, 31.7),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
-    },
-    -- To downstairs (double doors)
-    {
-        textCoords = vector3(444.6, -989.4, 31.7),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 4,
-        doors = {
-            {objHash = GetHashKey("v_ilev_ph_gendoor005"), objHeading = 180.0, objCoords = vector3(443.9, -989.0, 30.6)},
-            {objHash = GetHashKey("v_ilev_ph_gendoor005"), objHeading = 0.0, objCoords = vector3(445.3, -988.7, 30.6)}
-        }
-    },
-    --
-    -- Mission Row Cells
-    --
+   --  {
+    --     textCoords = vector3(434.7, -982.0, 31.5),
+    --     authorizedJobs = {"police", "lssd"},
+    --     locked = false,
+    --     maxDistance = 2.5,
+    --     doors = {
+    --         {objHash = GetHashKey("v_ilev_ph_door01"), objHeading = 270.0, objCoords = vector3(434.7, -980.6, 30.8)},
+    --         {objHash = GetHashKey("v_ilev_ph_door002"), objHeading = 270.0, objCoords = vector3(434.7, -983.2, 30.8)}
+    --     }
+    -- },
+    -- -- To locker room & roof
+    -- {
+    --     objHash = GetHashKey("v_ilev_ph_gendoor004"),
+    --     objHeading = 90.0,
+    --     objCoords = vector3(449.6, -986.4, 30.6),
+    --     textCoords = vector3(450.1, -986.3, 31.7),
+    --     authorizedJobs = {"police", "lssd"},
+    --     locked = true,
+    --     maxDistance = 1.25
+    -- },
+    -- -- Rooftop
+    -- {
+    --     objHash = GetHashKey("v_ilev_gtdoor02"),
+    --     objHeading = 90.0,
+    --     objCoords = vector3(464.3, -984.6, 43.8),
+    --     textCoords = vector3(464.3, -984.0, 44.8),
+    --     authorizedJobs = {"police", "lssd"},
+    --     locked = true,
+    --     maxDistance = 1.25
+    -- },
+    -- -- Hallway to roof
+    -- {
+    --     objHash = GetHashKey("v_ilev_arm_secdoor"),
+    --     objHeading = 90.0,
+    --     objCoords = vector3(461.2, -985.3, 30.8),
+    --     textCoords = vector3(461.5, -986.0, 31.5),
+    --     authorizedJobs = {"police", "lssd"},
+    --     locked = true,
+    --     maxDistance = 1.25
+    -- },
+    -- -- Armory
+    -- {
+    --     objHash = GetHashKey("v_ilev_arm_secdoor"),
+    --     objHeading = 270.0,
+    --     objCoords = vector3(452.6, -982.7, 30.6),
+    --     textCoords = vector3(453.0, -982.6, 31.7),
+    --     authorizedJobs = {"police", "lssd"},
+    --     locked = true,
+    --     maxDistance = 1.25
+    -- },
+    -- -- Captain Office
+    -- {
+    --     objHash = GetHashKey("v_ilev_ph_gendoor002"),
+    --     objHeading = 180.0,
+    --     objCoords = vector3(447.2, -980.6, 30.6),
+    --     textCoords = vector3(447.2, -980.0, 31.7),
+    --     authorizedJobs = {"police", "lssd"},
+    --     locked = true,
+    --     maxDistance = 1.25
+    -- },
+    -- -- To downstairs (double doors)
+    -- {
+    --     textCoords = vector3(444.6, -989.4, 31.7),
+    --     authorizedJobs = {"police", "lssd"},
+    --     locked = true,
+    --     maxDistance = 4,
+    --     doors = {
+    --         {objHash = GetHashKey("v_ilev_ph_gendoor005"), objHeading = 180.0, objCoords = vector3(443.9, -989.0, 30.6)},
+    --         {objHash = GetHashKey("v_ilev_ph_gendoor005"), objHeading = 0.0, objCoords = vector3(445.3, -988.7, 30.6)}
+    --     }
+    -- },
+    -- --
+    -- -- Mission Row Cells
+    -- --
 
     -- Main Cells
     {
-        objHash = GetHashKey("v_ilev_ph_cellgate"),
-        objHeading = 0.0,
-        objCoords = vector3(463.8, -992.6, 24.9),
-        textCoords = vector3(463.3, -992.6, 25.1),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
-    },
-    -- Cell 1
-    {
-        objHash = GetHashKey("v_ilev_ph_cellgate"),
+        objHash = -801376572,
         objHeading = 270.0,
-        objCoords = vector3(462.3, -993.6, 24.9),
-        textCoords = vector3(461.8, -993.3, 25.0),
+        objCoords = vector3(484.32, -1004.28, 26.21),
+        textCoords = vector3(484.32, -1004.28, 26.21),
         authorizedJobs = {"police", "lssd"},
         locked = true,
-        maxDistance = 1.25
+        maxDistance = 1.0
     },
-    -- Cell 2
     {
-        objHash = GetHashKey("v_ilev_ph_cellgate"),
+        objHash = -801376572,
+        objHeading = 270.0,
+        objCoords = vector3(484.39, -1006.68, 26.21),
+        textCoords = vector3(484.39, -1006.68, 26.21),
+        authorizedJobs = {"police", "lssd"},
+        locked = true,
+        maxDistance = 1.0
+    },
+    {
+        objHash = -801376572,
+        objHeading = 270.0,
+        objCoords = vector3(484.43, -1009.51, 26.21),
+        textCoords = vector3(484.43, -1009.51, 26.21),
+        authorizedJobs = {"police", "lssd"},
+        locked = true,
+        maxDistance = 1.0
+    },
+    {
+        objHash = -801376572,
+        objHeading = 270.0,
+        objCoords = vector3(484.38, -1012.07, 26.21),
+        textCoords = vector3(484.38, -1012.07, 26.21),
+        authorizedJobs = {"police", "lssd"},
+        locked = true,
+        maxDistance = 0.5
+    },
+    {
+        objHash = -801376572,
+        objHeading = 180.0,
+        objCoords = vector3(483.5, -1012.49, 26.21),
+        textCoords = vector3(483.5, -1012.49, 26.21),
+        authorizedJobs = {"police", "lssd"},
+        locked = true,
+        maxDistance = 0.5
+    },
+    {
+        objHash = -801376572,
+        objHeading = 180.0,
+        objCoords = vector3(480.89, -1012.49, 26.21),
+        textCoords = vector3(480.89, -1012.49, 26.21),
+        authorizedJobs = {"police", "lssd"},
+        locked = true,
+        maxDistance = 1.0
+    },
+    {
+        objHash = -801376572,
+        objHeading = 180.0,
+        objCoords = vector3(478.29, -1012.49, 26.21),
+        textCoords = vector3(478.29, -1012.49, 26.21),
+        authorizedJobs = {"police", "lssd"},
+        locked = true,
+        maxDistance = 1.0
+    },
+    {
+        objHash = -801376572,
         objHeading = 90.0,
-        objCoords = vector3(462.3, -998.1, 24.9),
-        textCoords = vector3(461.8, -998.8, 25.0),
+        objCoords = vector3(477.74, -1006.54, 26.21),
+        textCoords = vector3(477.74, -1006.54, 26.21),
         authorizedJobs = {"police", "lssd"},
         locked = true,
-        maxDistance = 1.25
-    },
-    -- Cell 3
-    {
-        objHash = GetHashKey("v_ilev_ph_cellgate"),
-        objHeading = 90.0,
-        objCoords = vector3(462.7, -1001.9, 24.9),
-        textCoords = vector3(461.8, -1002.4, 25.0),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
-    },
-    -- To Back
-    {
-        objHash = GetHashKey("v_ilev_gtdoor"),
-        objHeading = 0.0,
-        objCoords = vector3(463.4, -1003.5, 25.0),
-        textCoords = vector3(464.0, -1003.5, 25.5),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 1.25
+        maxDistance = 1.0
     },
     --
     -- Mission Row Back
@@ -141,26 +164,26 @@ Config.DoorList = {
 
     -- Back (double doors)
     {
-        textCoords = vector3(468.6, -1014.4, 27.1),
+        textCoords = vector3(457.07, -972.75, 30.71),
         authorizedJobs = {"police", "lssd"},
         locked = true,
         maxDistance = 2.25,
         doors = {
-            {objHash = GetHashKey("v_ilev_rc_door2"), objHeading = 0.0, objCoords = vector3(467.3, -1014.4, 26.5)},
-            {objHash = GetHashKey("v_ilev_rc_door2"), objHeading = 180.0, objCoords = vector3(469.9, -1014.4, 26.5)}
+            {objHash = 1337093550, objHeading = 180.0, objCoords = vector3(456.48, -972.77, 30.71)},
+            {objHash = 1337093550, objHeading = 0.0, objCoords = vector3(457.62, -972.76, 30.71)}
+        }
+    },
+    {
+        textCoords = vector3(468.57, -1014.99, 26.39),
+        authorizedJobs = {"police", "lssd"},
+        locked = true,
+        maxDistance = 2.25,
+        doors = {
+            {objHash = -1200630391, objHeading = 0.0, objCoords = vector3(468.0, -1015.02, 26.39)},
+            {objHash = -1200630391, objHeading = 180.0, objCoords = vector3(469.61, -1015.05, 26.39)}
         }
     },
     -- Back Gate
-    {
-        objHash = GetHashKey("hei_prop_station_gate"),
-        objHeading = 90.0,
-        objCoords = vector3(488.8, -1017.2, 27.1),
-        textCoords = vector3(488.8, -1020.2, 30.0),
-        authorizedJobs = {"police", "lssd"},
-        locked = true,
-        maxDistance = 14,
-        size = 2
-    }, ]]
     --
     -- Sandy Shores
     --
