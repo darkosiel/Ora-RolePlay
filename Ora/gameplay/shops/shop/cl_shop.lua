5cec9b26ff4794a81d75f3374fd89c627d6c5a92
PaoloOra <dastara33@gmail.com>
2023-01-04 17:29:34 +0100

    fix linda

--- Ora/gameplay/shops/shop/cl_shop.lua
+++ Ora/gameplay/shops/shop/cl_shop.lua
@@ -563,7 +563,7 @@ local Shops =
     {
         {
             Title = "Préfecture",
-            Pos = {x = 837.0902, y = -1292.1562, z = 27.2331, a = 65.7923},
+            Pos = {x = 440.9636, y = -981.0509, z = 29.6894, a = 115.8658},
             Blips = {
                 sprite = nil,
                 color = 17,
@@ -572,6 +572,34 @@ local Shops =
             Ped = {
                 model = "a_f_y_femaleagent",
                 name = "Linda"
+            },
+            Menus = {
+                Sprite = "shopui_title_conveniencestore",
+                Enabled = false
+            },
+            braquable = false,
+            Hidden = true,
+            Items = {
+                {
+                    name = "identity",
+                    price = 15,
+                    data = {},
+                    type = "identity",
+                    fct = GetIdentity
+                }
+            }
+        },
+        {
+            Title = "Préfecture",
+            Pos = {x = 482.9660, y = -987.7573, z = 25.2075, a = 86.2769},
+            Blips = {
+                sprite = nil,
+                color = 17,
+                name = "Préfecture"
+            },
+            Ped = {
+                model = "a_f_y_femaleagent",
+                name = "Landi"
             },
             Menus = {
                 Sprite = "shopui_title_conveniencestore",
@@ -4134,186 +4162,6 @@ local Shops =
 --                 -- },
 --             }
 --         },
-        {
-            Title = "Fournisseur USMS",
-            Hidden = true,
-            RestrictedJob = {"usms"},
-            RestrictedGrade = {"drh", "boss"},
-            Pos = {x = 480.32, y = -996.76, z = 30.68, a = 86.11}, -- { x = 454.1, y = -980.07, z = 29.69, a = 85.87 },
-            Blips = {
-                sprite = 478,
-                Enabled = false, -- Image off
-                color = 43,
-                name = "USMS"
-            },
-            Ped = {
-                model = "s_m_m_fibsec_01",
-                name = "Bob"
-            },
-            Menus = {
-                Sprite = "shopui_title_conveniencestore",
-                Enabled = false
-            },
-            braquable = false,
-            Items = {
-                {
-                    name = "menottes",
-                    price = 70,
-                    data = {}
-                },
-                {
-                    name = "radio",
-                    price = 80,
-                    data = {}
-                },
-                {
-                    name = "mm9",
-                    price = 2,
-                    data = {}
-                },
-                {
-                    name = "cab2",
-                    price = 2,
-                    data = {}
-                },
-                {
-                    name = "calibre12",
-                    price = 2,
-                    data = {}
-                },
-                {
-                    name = "acp45",
-                    price = 2,
-                    data = {}
-                },
-                -- {
-                --     name = "pistol",
-                --     price = 1200,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type =  "serial"
-                -- },
-                -- {
-                --     name = "pistolcombat",
-                --     price = 1890,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                -- {
-                --     name = "pistol50",
-                --     price = 1560,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type =  "serial"
-                -- },
-                -- {
-                --     name = "stungun",
-                --     price = 530,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                {
-                    name = "flaregun",
-                    price = 250,
-                    data = {},
-                    fct = GeneratePoliceSerial,
-                    type = "serial"
-                },
-                -- {
-                --     name = "assaultsmg",
-                --     price = 15000,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type =  "serial"
-                -- },
-                -- {
-                --     name = "nightstick",
-                --     price = 50,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                -- {
-                --     name = "teargas",
-                --     price = 75,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                -- {
-                --     name = "carrabine",
-                --     price = 20000,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                -- {
-                --     name = "beanbag",
-                --     price = 3400,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                -- {
-                --     name = "shootgun",
-                --     price = 4300,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                -- {
-                --     name = "smg",
-                --     price = 14000,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                -- },
-                -- {
-                --     name = "kevlar",
-                --     price = 3000,
-                --     data = {status = 25, showLabel = "Kevlar Léger", label = "Léger"},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                --   },
-                --   {
-                --     name = "kevlar",
-                --     price = 6000,
-                --     data = {status = 50, showLabel = "Kevlar Renforcé", label = "Renforcé"},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                --   },
-                --   {
-                --     name = "kevlar",
-                --     price = 9000,
-                --     data = {status = 75, showLabel = "Kevlar SWAT", label = "SWAT"},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                --   },
-                --   {
-                --     name = "kevlar",
-                --     price = 12000,
-                --     data = {status = 100, showLabel = "Kevlar Militaire", label = "Militaire"},
-                --     fct = GeneratePoliceSerial,
-                --     type = "serial"
-                --   }
-                -- {
-                --     name = "heavysniper",
-                --     price = 50000,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type =  "serial"
-                -- },
-                -- {
-                --     name = "compactrifle",
-                --     price = 25000,
-                --     data = {},
-                --     fct = GeneratePoliceSerial,
-                --     type =  "serial"
-                -- },
-            }
-        },
         --[[{
             Title = "Annonce Weazel News",
             Hidden = true,
