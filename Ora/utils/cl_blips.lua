c1087bc24eebc883d918c34f62d6a9b5bba5ccb0
PaoloOra <dastara33@gmail.com>
2023-01-15 19:07:15 +0100

    add KribySness

--- Ora/utils/cl_blips.lua
+++ Ora/utils/cl_blips.lua
@@ -60,7 +60,7 @@ local Blips = {
     {name = "Coiffeur", color = 64, sprite = 71, Pos = {x = 138.25, y = -1709.13, z = 29.42}},
     {name = "Barber", color = 64, sprite = 71, Pos = {x = -36.48, y = -155.96, z = 57.07}},
     -- {name = "Ponsonbys", color = 64, sprite = 366, Pos = {x = -716.84, y = -156.75, z = 36.98}},
-    {name = "Binco", color = 64, sprite = 366, Pos = {x = -829.413, y = -1073.710, z = 10.328}},
+    {name = "KribySness", color = 64, sprite = 366, Pos = {x = -829.413, y = -1073.710, z = 10.328}},
     --{name = "Agence immobilière", color = 64, sprite = 492, Pos = {x = -707.79388, y = 269.2157, z = 94.2941}},
     --{name = "Mirror Restaurant", color = 3, sprite = 304, Pos = {x = -1342.2145, y = -1075.8182, z = 6.93}},
     {name = "Burger Shot", color = 75, sprite = 106, Pos = {x = -1182.10, y = -883.58, z = 13.78}},
