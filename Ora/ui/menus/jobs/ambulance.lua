eecb20944978307c41a08392551fc0d70cac4e39
Zelda601 <tom.dupont6470@gmail.com>
2023-03-27 15:20:39 +0200

    Fix brancard Pompier

--- Ora/ui/menus/jobs/ambulance.lua
+++ Ora/ui/menus/jobs/ambulance.lua
@@ -172,7 +172,7 @@ end
 function Pompier.GetOffStretcher()
     local stretcherHash = GetHashKey("prop_ld_binbag_01")
     local veh = GetVehicleInDirection()
-    if veh ~= 0 and GetEntityModel(veh) == GetHashKey("lsfd4") or GetEntityModel(veh) == GetHashKey("lsfd3") then
+    if veh ~= 0 and GetEntityModel(veh) == GetHashKey("samsbisonamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb2") or GetEntityModel(veh) == GetHashKey("Romero") or GetEntityModel(veh) == GetHashKey("medevac") or GetEntityModel(veh) == GetHashKey("lsfdbisonamb") or GetEntityModel(veh) == GetHashKey("lsfdbisoncmd") or GetEntityModel(veh) == GetHashKey("lsfdbison2") then
         local vehCoords = GetEntityCoords(veh)
         local forward = GetEntityForwardVector(veh)
         local behind = vehCoords - (forward * 6.0)
@@ -200,7 +200,7 @@ function Ambulance.PutStretcherOnVehicle()
     local stretcher = GetClosestObjectOfType(coords, 3.0, stretcherHash)
     if stretcher and GetEntityAttachedTo(stretcher) == ped then
         local veh = GetVehicleInDirection()
-        if veh ~= 0 and GetEntityModel(veh) == GetHashKey("samsbisonamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb2") or GetEntityModel(veh) == GetHashKey("Romero") or GetEntityModel(veh) == GetHashKey("medevac") then
+        if veh ~= 0 and GetEntityModel(veh) == GetHashKey("samsbisonamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb2") or GetEntityModel(veh) == GetHashKey("Romero") or GetEntityModel(veh) == GetHashKey("medevac") or GetEntityModel(veh) == GetHashKey("lsfdbisonamb") or GetEntityModel(veh) == GetHashKey("lsfdbisoncmd") or GetEntityModel(veh) == GetHashKey("lsfdbison2") then
             DetachEntity(stretcher)
             AttachEntityToEntity(stretcher, veh, 0.0, 0.0, -2.0, 0.8, 0.0, 0.0, 0.0, false, false, false, false, 2, true)
             FreezeEntityPosition(stretcher, true)
@@ -217,7 +217,7 @@ function Pompier.PutStretcherOnVehicle()
     local stretcher = GetClosestObjectOfType(coords, 3.0, stretcherHash)
     if stretcher and GetEntityAttachedTo(stretcher) == ped then
         local veh = GetVehicleInDirection()
-        if veh ~= 0 and GetEntityModel(veh) == GetHashKey("lsfd4") or GetEntityModel(veh) == GetHashKey("lsfd3") then
+        if veh ~= 0 and GetEntityModel(veh) == GetHashKey("samsbisonamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb") or GetEntityModel(veh) == GetHashKey("samsspeedoamb2") or GetEntityModel(veh) == GetHashKey("Romero") or GetEntityModel(veh) == GetHashKey("medevac") or GetEntityModel(veh) == GetHashKey("lsfdbisonamb") or GetEntityModel(veh) == GetHashKey("lsfdbisoncmd") or GetEntityModel(veh) == GetHashKey("lsfdbison2") then
             DetachEntity(stretcher)
             AttachEntityToEntity(stretcher, veh, 0.0, 0.0, -2.0, 0.8, 0.0, 0.0, 0.0, false, false, false, false, 2, true)
             FreezeEntityPosition(stretcher, true)
